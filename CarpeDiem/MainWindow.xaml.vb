2341a06614e0b47663cab78491a2f57267cdae33
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-10-12 21:44:31 +0500

    added timer and progressbar

--- CarpeDiem/MainWindow.xaml.vb
+++ CarpeDiem/MainWindow.xaml.vb
@@ -1,3 +1,19 @@
-﻿Class MainWindow 
+﻿Imports System.Windows.Threading
 
+Class MainWindow
+
+
+    dim timer1 as New DispatcherTimer
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        AddHandler timer1.Tick, AddressOf timer1_Tick
+        timer1.Interval = new TimeSpan(0,0,1)
+        timer1.Start()
+    End Sub
+
+    Private sub timer1_Tick(sender As Object, e As EventArgs)
+        Progressbar1.Value += 10
+        If Progressbar1.Value >= 100 then
+            timer1.IsEnabled = False
+        End If
+    End sub
 End Class
