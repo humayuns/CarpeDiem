84722c7ac8e077274ca9e1706510850f25fae69f
Humayun Shabbir <humayun_shabbir@hotmail.com>
2015-11-06 18:11:55 +0500

    added code to check time difference

--- CarpeDiem/MainWindow.xaml.vb
+++ CarpeDiem/MainWindow.xaml.vb
@@ -2,7 +2,8 @@
 
 Class MainWindow
 
-    dim timer1 as New DispatcherTimer
+    Dim timer1 As New DispatcherTimer
+    Dim timeDiff As Date
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
 
     End Sub
@@ -13,11 +14,27 @@ Class MainWindow
             timer1.IsEnabled = False
             Progressbar1.Value = 0
         End If
-    End sub
+    End Sub
 
     Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+
+        ' set up timer and start it
         AddHandler timer1.Tick, AddressOf timer1_Tick
-        timer1.Interval = new TimeSpan(0,0,1)
+        timer1.Interval = New TimeSpan(0, 0, 1)
         timer1.Start()
+
+        timeDiff = Date.Now.AddHours(textBoxHours.Text)
+
+        MsgBox(timeDiff.ToString(), MsgBoxStyle.Information)
+
+        Dim diff = timeDiff.Subtract(Date.Now)
+        Debug.WriteLine(diff)
+        MsgBox(diff.TotalMinutes, MsgBoxStyle.Information)
+
     End Sub
+
+    Private Function GetHoursDifference() As Integer
+        'get the number of hours
+        Return 0
+    End Function
 End Class
