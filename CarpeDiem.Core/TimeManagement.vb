1786064297827aec2cbae7cbcb1d37826f1b0241
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-02-29 19:46:10 +0500

    added function TotalMinutesAfter

--- CarpeDiem.Core/TimeManagement.vb
+++ CarpeDiem.Core/TimeManagement.vb
@@ -2,10 +2,11 @@
 
 
     Public Shared Function TimeAfterHours(hours As Integer) As Date
-
-
         Return Date.Now.AddHours(hours)
+    End Function
 
+    Public Shared Function TotalMinutesAfter(hours As Integer) As Integer
+        Return TimeAfterHours(hours).Subtract(Date.Now).TotalMinutes
     End Function
 
 
--- CarpeDiem.Tests/TimeManagementTest.vb
+++ CarpeDiem.Tests/TimeManagementTest.vb
@@ -9,4 +9,12 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         Assert.AreEqual(TimeManagement.TimeAfterHours(3), Now.AddHours(3))
     End Sub
 
+    <TestMethod()> Public Sub TotalMinutesAfter2Hours()
+
+        Dim totalMinutesAfter = TimeManagement.TotalMinutesAfter(2)
+        Dim totalMinutesAfterTest = CInt(TimeManagement.TimeAfterHours(2).Subtract(Date.Now).TotalMinutes)
+        Debug.Print(totalMinutesAfterTest.ToString())
+        Assert.AreEqual(totalMinutesAfter, totalMinutesAfterTest)
+    End Sub
+
 End Class
