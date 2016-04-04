ba3d90304ccbf0f068381a5daea35b7917f0a950
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-04-05 02:44:46 +0500

    added initial code for Focus window.

--- CarpeDiem/Focus.xaml.vb
+++ CarpeDiem/Focus.xaml.vb
@@ -1,3 +1,24 @@
-﻿Public Class Focus
+﻿Imports System.Windows.Threading
 
+Public Class Focus
+
+    ReadOnly timer1 As New DispatcherTimer
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        AddHandler timer1.Tick, AddressOf timer1_Tick
+    End Sub
+
+    Private Sub timer1_Tick(sender As Object, e As EventArgs)
+        If ProgressBar1.Value > 0 Then
+            ProgressBar1.Value -= 1
+        Else
+            timer1.Stop()
+        End If
+    End Sub
+
+    Private Sub buttonStart_Click(sender As Object, e As RoutedEventArgs) Handles buttonStart.Click
+        timer1.Interval = New TimeSpan(0, 0, 1)
+        timer1.Start()
+        ProgressBar1.Value = 100
+    End Sub
 End Class
