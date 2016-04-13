eadb7acda1aba72c91e81470afa338030c07f938
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-04-13 20:02:32 +0500

    Added AbilityTransparent.

--- CarpeDiem/Ability.xaml.vb
+++ CarpeDiem/Ability.xaml.vb
@@ -5,16 +5,21 @@ Public Class Ability
     ReadOnly timer1 As New DispatcherTimer
     Dim targetTime As New Date
     Dim totalSeconds As Integer
+    Dim abilityTransparent As AbilityTransparent
 
     Private Sub Ability_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         AddHandler timer1.Tick, AddressOf timer1_Tick
         timer1.Interval = New TimeSpan(0, 0, 1)
-
+        If abilityTransparent Is Nothing Then
+            abilityTransparent = New AbilityTransparent
+            abilityTransparent.ParentWindow = Me
+        End If
     End Sub
 
     Private Sub timer1_Tick(sender As Object, e As EventArgs)
         ' change progress bar value according to time difference
         ProgressBar1.Value = (100 * targetTime.Subtract(Date.Now).TotalSeconds / totalSeconds)
+
         Debug.Print(ProgressBar1.Value)
         If ProgressBar1.Value = 0 Then
             timer1.Stop()
@@ -32,5 +37,13 @@ Public Class Ability
         totalSeconds = targetTime.Subtract(Date.Now).TotalSeconds
         ProgressBar1.Value = 100
         timer1.Start()
+    End Sub
+
+    Private Sub image_MouseRightButtonUp(sender As Object, e As MouseButtonEventArgs) Handles image.MouseRightButtonUp
+
+
+
+        abilityTransparent.Show()
+        Me.Hide()
     End Sub
 End Class
---
+++ CarpeDiem/AbilityTransparent.xaml.vb
@@ -0,0 +1,13 @@
+﻿Public Class AbilityTransparent
+
+    Public Property ParentWindow As Window
+
+    Private Sub Window_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        If (e.ChangedButton = MouseButton.Left) Then DragMove()
+    End Sub
+
+    Private Sub image_MouseRightButtonUp(sender As Object, e As MouseButtonEventArgs) Handles image.MouseRightButtonUp
+        Me.Hide()
+        Me.ParentWindow.Show()
+    End Sub
+End Class
