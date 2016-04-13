b80fc8b6b2e519629402bd69c2967498522f7ac5
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-04-13 15:06:00 +0500

    added code for DragMove

--- CarpeDiem/Ability.xaml.vb
+++ CarpeDiem/Ability.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Ability
-
+    Private Sub Window_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        If (e.ChangedButton = MouseButton.Left) Then DragMove()
+    End Sub
 End Class
