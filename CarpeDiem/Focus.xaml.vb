96b318f67109cc61acdce029f6df293aab135210
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-04-23 20:19:42 +0500

    modified focus code

--- CarpeDiem/Focus.xaml.vb
+++ CarpeDiem/Focus.xaml.vb
@@ -9,17 +9,19 @@ Public Class Focus
     End Sub
 
     Private Sub timer1_Tick(sender As Object, e As EventArgs)
-        If ProgressBar1.Value > 0 Then
-            ProgressBar1.Value -= 1
+        If ProgressBar1.Value < 100 Then
+            ProgressBar1.Value += 1
         Else
             timer1.Stop()
         End If
     End Sub
 
     Private Sub buttonStart_Click(sender As Object, e As RoutedEventArgs) Handles buttonStart.Click
-        timer1.Interval = New TimeSpan(0, 0, 1)
+        timer1.Interval = New TimeSpan(0, 0, 0, 100)
+        timer1.IsEnabled = True
         timer1.Start()
-        ProgressBar1.Value = 100
+        ProgressBar1.Value = 0
         buttonStart.Content = "Stop"
     End Sub
+
 End Class
