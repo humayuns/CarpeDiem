fef17009ab2f6a04e7166469cbad6753aaada071
Humayun Shabbir <humayun_shabbir@hotmail.com>
2016-05-05 02:41:15 +0500

    layout improvement and fixed progress bar

--- CarpeDiem/CountDown.xaml.vb
+++ CarpeDiem/CountDown.xaml.vb
@@ -1,9 +1,11 @@
 ﻿Imports System.Windows.Threading
+Imports CarpeDiem.Core
 
 Public Class CountDown
 
     ReadOnly timer1 As New DispatcherTimer
     Private targetSeconds As Integer
+    Private startTime As Date
     Private targetTime As Date
     Dim tspn As New TimeSpan()
 
@@ -13,11 +15,12 @@ Public Class CountDown
 
     Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
         targetSeconds = textBoxTarget.Text
+        startTime = Now
         targetTime = Now.AddSeconds(targetSeconds)
 
         tspn = New TimeSpan(0, 0, targetSeconds)
 
-        Timer1.Start
+        timer1.Start
         button.IsEnabled = False
 
     End Sub
@@ -30,9 +33,10 @@ Public Class CountDown
     Private Sub timer1_Tick(sender As Object, e As EventArgs)
         tspn = tspn.Subtract(New TimeSpan(0, 0, 1))
 
+        ProgressBar1.Value = TimeManagement.GetDifferencePercentage(startTime, targetTime)
         labelTime.Content = String.Format(" {0} Mins : {1} Secs", tspn.Minutes, tspn.Seconds)
         If tspn.Minutes = 0 AndAlso tspn.Seconds = 0 Then
-            Timer1.Stop()
+            timer1.Stop()
             button.IsEnabled = True
         End If
 
